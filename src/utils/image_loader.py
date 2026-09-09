2a51be5a0306589b81d5d2290876e46454327027
IrishJohnnyP <pietrowicz@ameritech.net>
2026-09-09 17:26:14 -0400

    Increase saturation values for display profiles

--- src/utils/image_loader.py
+++ src/utils/image_loader.py
@@ -60,13 +60,13 @@ class AdaptiveImageLoader:
         # on Pimoroni Spectra 6 displays.
         self.display_profiles = {
             (1600, 1200): { # 13.3" Spectra 6
-                "saturation": 0.6,   
+                "saturation": 1.0,   
                 "contrast": 1.4,     
                 "brightness": 1.0,   
                 "sharpness": 2.0
             },
             (800, 480): {   # 7.3" Spectra 6
-                "saturation": 0.6,
+                "saturation": 1.0,
                 "contrast": 1.4,
                 "brightness": 1.0,
                 "sharpness": 2.0
